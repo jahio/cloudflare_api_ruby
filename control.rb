cc6c881caa29b4bfc65b66aaebf27037da7049a5
J. Austin Hughey <jaustinhughey@me.com>
2014-03-07 21:38:51 -0600

    first crack at a basic control script

---
+++ control.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/env ruby
+require 'rubygems'
+require 'bundler'
+
+Bundler.require # bring in all the gems
+
+# Require our own libraries.and credentials
+require File.join(Dir.pwd, 'lib', 'api_wrapper.rb')
+require File.join(Dir.pwd, 'config', 'creds.rb')
+
+binding.pry
