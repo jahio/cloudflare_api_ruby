72d149c0dac36d30ca116ff46dcc4e48c85f1801
J. Austin Hughey <jaustinhughey@me.com>
2014-03-07 22:13:21 -0600

    first implimentation of post method; untested, ran into issues with CF API

--- lib/api_wrapper.rb
+++ lib/api_wrapper.rb
@@ -28,4 +28,32 @@ class ApiWrapper
     )
     return response.run
   end
+
+  #
+  # ApiWrapper.post("rec_new", {z: "example.com", type: "A", name: "mysub",
+  #                  content: "1.2.3.4"})
+  #
+  # Performs a POST request to the API for the action (first parameter) and with
+  # the parameters in the supplied hash. As for what to send up, for that you
+  # need the API docs: http://www.cloudflare.com/docs/client-api.html#s1.1
+  # The example above adds a subdomain called "mysub" (which would resolve to
+  # "mysub.example.com") with the IP address of "1.2.3.4".
+  def self.post(action, params = {})
+    response = Typhoeus::Request.new(
+      ENDPOINT,
+      method: :post,
+      params: params.merge(AUTH).merge({a: action.to_s}).each do |k,v|
+        # Force the value into a string, juuuuust in case...
+        v = v.to_s
+      end
+    )
+    return response.run
+  end
+  # Those of you reading the code, at this point, are probably wondering, "why
+  # do you have two separate get/post methods that are identical?"
+  # Answer: as a matter of basic design. I want to keep them separate *from
+  # the get-go* so that if I need to change the behavior of one in the future,
+  # we don't have anybody already used to throwing GET/POST requests around
+  # interchangably, and hopefully have it ingrained that they are indeed
+  # different methods with different purposes.
 end
